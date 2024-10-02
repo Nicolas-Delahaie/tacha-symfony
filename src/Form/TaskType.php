c0dbfebbeb45581dd63055c896868c1753497a1a
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2024-10-02 10:52:20 +0200

    Création d'un crud

---
+++ src/Form/TaskType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('start_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('start_time', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('title')
+            ->add('description')
+            ->add('is_hidden')
+            ->add('is_achieved')
+            ->add('priority')
+            ->add('desire')
+            ->add('concentration')
+            ->add('workload')
+            ->add('desired_deadline')
+            ->add('deadline', null, [
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
