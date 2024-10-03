7c5444a23c367ec74e780e3b62adf13a694791df
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2024-10-03 16:52:51 +0200

    Maj du crud de Task + ajout visuel des relations

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Tag;
 use App\Entity\Task;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -27,7 +29,18 @@ class TaskType extends AbstractType
             ->add('concentration')
             ->add('workload')
             ->add('deadline', null, [
-                'widget' => 'choice',
+                'widget' => 'single_text',
+            ])
+            ->add('father', EntityType::class, [
+                'class' => Task::class,
+                'choice_label' => 'title',
+                'required' => false, // Rendre ce champ facultatif
+            ])
+            ->add('tags', EntityType::class, [
+                'class' => Tag::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'required' => false, // Rendre ce champ facultatif
             ])
         ;
     }
